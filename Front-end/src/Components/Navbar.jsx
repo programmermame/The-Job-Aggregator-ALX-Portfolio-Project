26509e2efde74d99e481d75289ef6bb26204dbd5
programmermame <mameatpickout@gmail.com>
2024-05-05 17:08:07 +0300

    front-end

--- Front-end/src/Components/Navbar.jsx
+++ Front-end/src/Components/Navbar.jsx
@@ -1,14 +1,17 @@
 import logo from '../assets/Images/JOB.png'
 
 const Navbar = () => {
-  return (
-    <div className='bg-black text-white'>
-        <a href=""><img className='w-10 h-10 ' src={logo} alt="logo" /></a>
-        <a href="">Dashboard</a>
-        <a href="">Find Job</a>
+    return (
+        <div className='bg-black text-white flex'>
+            <a href=""><img className='w-10 h-10 ' src={logo} alt="logo" /></a>
+            <div className='flex gap-5 justify-center items-center ml-auto mr-auto'>
+                <a className='hover:bg-white hover:text-black p-1 rounded-lg' href="">Dashboard</a>
+                <a className='hover:bg-white hover:text-black p-1 rounded-lg' href="">Find Job</a>
+                <a className='hover:bg-white hover:text-black p-1 rounded-lg' href="">About</a>
+            </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Navbar
