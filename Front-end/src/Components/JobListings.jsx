05f1535aa42effe9f6036cd7d6401e75e87a7c7c
programmermame <mameatpickout@gmail.com>
2024-05-05 23:27:52 +0300

    front-end

--- Front-end/src/Components/JobListings.jsx
+++ Front-end/src/Components/JobListings.jsx
@@ -5,14 +5,14 @@ const JobListings = () => {
     return (
         <div className='grid grid-cols-1 md:grid-cols-3 gap-4 p-3'>
             {/* Job Listing 1 */}
-            <div className="flex border shadow-md p-4">
+            <div className="flex border shadow-xl p-4 rounded-lg bg-gray-50 hover:bg-white">
                 <div className="w-16 h-16 bg-green-200"></div>
                 <div className="ml-5">
                     <h2 className="font-extrabold">Barista</h2>
                     <p>Eleven Madison Park</p>
                     <p className='text-gray-600'>New York, NY</p>
                     <p className='mb-4 text-gray-600'>via Indeed</p>
-                    <div className='flex gap-4'>
+                    <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FiClock className="w-4 h-4" />
                             <p className='ml-1'>23 hours ago</p>
@@ -22,7 +22,7 @@ const JobListings = () => {
                             <p className='ml-1'>Full-time</p>
                         </div>
                     </div>
-                    <div className='flex gap-4'>
+                    <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FaMinus className="w-4 h-4" />
                             <p className='ml-1'>No degree mentioned</p>
@@ -36,14 +36,14 @@ const JobListings = () => {
             </div>
 
             {/* Job Listing 2 */}
-            <div className="flex border shadow-md p-4">
+            <div className="flex border shadow-xl p-4 rounded-lg bg-gray-50 hover:bg-white">
                 <div className="w-16 h-16 bg-green-200"></div>
                 <div className="ml-5">
                     <h2 className="font-extrabold">Barista</h2>
                     <p>Eleven Madison Park</p>
                     <p className='text-gray-600'>New York, NY</p>
                     <p className='mb-4 text-gray-600'>via Indeed</p>
-                    <div className='flex gap-4'>
+                    <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FiClock className="w-4 h-4" />
                             <p className='ml-1'>23 hours ago</p>
@@ -53,7 +53,7 @@ const JobListings = () => {
                             <p className='ml-1'>Full-time</p>
                         </div>
                     </div>
-                    <div className='flex gap-4'>
+                    <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FaMinus className="w-4 h-4" />
                             <p className='ml-1'>No degree mentioned</p>
@@ -66,15 +66,15 @@ const JobListings = () => {
                 </div>
             </div>
 
-            {/* Job Listing 2 */}
-            <div className="flex border shadow-md p-4">
+            {/* Job Listing 3 */}
+            <div className="flex border shadow-xl p-4 rounded-lg bg-gray-50 hover:bg-white">
                 <div className="w-16 h-16 bg-green-200"></div>
                 <div className="ml-5">
                     <h2 className="font-extrabold">Barista</h2>
                     <p>Eleven Madison Park</p>
                     <p className='text-gray-600'>New York, NY</p>
                     <p className='mb-4 text-gray-600'>via Indeed</p>
-                    <div className='flex gap-4'>
+                    <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FiClock className="w-4 h-4" />
                             <p className='ml-1'>23 hours ago</p>
@@ -84,7 +84,100 @@ const JobListings = () => {
                             <p className='ml-1'>Full-time</p>
                         </div>
                     </div>
-                    <div className='flex gap-4'>
+                    <div className='flex gap-4 text-sm justify-center'>
+                        <div className='flex text-gray-600 items-center'>
+                            <FaMinus className="w-4 h-4" />
+                            <p className='ml-1'>No degree mentioned</p>
+                        </div>
+                        <div className='flex text-gray-600 items-center'>
+                            <FaShieldAlt className="w-4 h-4" />
+                            <p className='ml-1'>Health insurance</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Job Listing 3 */}
+            <div className="flex border shadow-xl p-4 rounded-lg bg-gray-50 hover:bg-white">
+                <div className="w-16 h-16 bg-green-200"></div>
+                <div className="ml-5">
+                    <h2 className="font-extrabold">Barista</h2>
+                    <p>Eleven Madison Park</p>
+                    <p className='text-gray-600'>New York, NY</p>
+                    <p className='mb-4 text-gray-600'>via Indeed</p>
+                    <div className='flex gap-4 text-sm justify-center'>
+                        <div className='flex text-gray-600 items-center'>
+                            <FiClock className="w-4 h-4" />
+                            <p className='ml-1'>23 hours ago</p>
+                        </div>
+                        <div className='flex text-gray-600 items-center'>
+                            <FiBriefcase className="w-4 h-4" />
+                            <p className='ml-1'>Full-time</p>
+                        </div>
+                    </div>
+                    <div className='flex gap-4 text-sm justify-center'>
+                        <div className='flex text-gray-600 items-center'>
+                            <FaMinus className="w-4 h-4" />
+                            <p className='ml-1'>No degree mentioned</p>
+                        </div>
+                        <div className='flex text-gray-600 items-center'>
+                            <FaShieldAlt className="w-4 h-4" />
+                            <p className='ml-1'>Health insurance</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Job Listing 3 */}
+            <div className="flex border shadow-xl p-4 rounded-lg bg-gray-50 hover:bg-white">
+                <div className="w-16 h-16 bg-green-200"></div>
+                <div className="ml-5">
+                    <h2 className="font-extrabold">Barista</h2>
+                    <p>Eleven Madison Park</p>
+                    <p className='text-gray-600'>New York, NY</p>
+                    <p className='mb-4 text-gray-600'>via Indeed</p>
+                    <div className='flex gap-4 text-sm justify-center'>
+                        <div className='flex text-gray-600 items-center'>
+                            <FiClock className="w-4 h-4" />
+                            <p className='ml-1'>23 hours ago</p>
+                        </div>
+                        <div className='flex text-gray-600 items-center'>
+                            <FiBriefcase className="w-4 h-4" />
+                            <p className='ml-1'>Full-time</p>
+                        </div>
+                    </div>
+                    <div className='flex gap-4 text-sm justify-center'>
+                        <div className='flex text-gray-600 items-center'>
+                            <FaMinus className="w-4 h-4" />
+                            <p className='ml-1'>No degree mentioned</p>
+                        </div>
+                        <div className='flex text-gray-600 items-center'>
+                            <FaShieldAlt className="w-4 h-4" />
+                            <p className='ml-1'>Health insurance</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Job Listing 3 */}
+            <div className="flex border shadow-xl p-4 rounded-lg bg-gray-50 hover:bg-white">
+                <div className="w-16 h-16 bg-green-200"></div>
+                <div className="ml-5">
+                    <h2 className="font-extrabold">Barista</h2>
+                    <p>Eleven Madison Park</p>
+                    <p className='text-gray-600'>New York, NY</p>
+                    <p className='mb-4 text-gray-600'>via Indeed</p>
+                    <div className='flex gap-4 text-sm justify-center'>
+                        <div className='flex text-gray-600 items-center'>
+                            <FiClock className="w-4 h-4" />
+                            <p className='ml-1'>23 hours ago</p>
+                        </div>
+                        <div className='flex text-gray-600 items-center'>
+                            <FiBriefcase className="w-4 h-4" />
+                            <p className='ml-1'>Full-time</p>
+                        </div>
+                    </div>
+                    <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FaMinus className="w-4 h-4" />
                             <p className='ml-1'>No degree mentioned</p>
