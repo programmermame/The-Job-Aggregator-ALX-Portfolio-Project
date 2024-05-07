27a12d60a494bba34428cd4537a46079b0dc9904
programmermame <mameatpickout@gmail.com>
2024-05-07 09:48:55 +0300

    front-end

--- Front-end/src/Components/JobListings.jsx
+++ Front-end/src/Components/JobListings.jsx
@@ -2,34 +2,46 @@ import { FiClock, FiBriefcase } from 'react-icons/fi';
 import { FaShieldAlt, FaMinus } from 'react-icons/fa';
 
 const JobListings = () => {
+    const data = {
+        title:'Barista',
+        subtitle:'Eleven Madison Park',
+        location:'New York, NY',
+        via:'via Indeed',
+        jobPost:'23 hours ago',
+        type:'Full Time',
+        extensions:[
+            'No degree mentioned',
+            'Health insurance'
+        ]
+    }
     return (
         <div className='grid grid-cols-1 md:grid-cols-3 gap-4 p-3'>
             {/* Job Listing 1 */}
             <div className="flex border shadow-xl p-4 rounded-lg bg-gray-50 hover:bg-white">
                 <div className="w-16 h-16 bg-green-200"></div>
                 <div className="ml-5">
-                    <h2 className="font-extrabold">Barista</h2>
+                    <h2 className="font-extrabold">{data.title}</h2>
                     <p>Eleven Madison Park</p>
-                    <p className='text-gray-600'>New York, NY</p>
-                    <p className='mb-4 text-gray-600'>via Indeed</p>
+                    <p className='text-gray-600'>{data.location}</p>
+                    <p className='mb-4 text-gray-600'>{data.via}</p>
                     <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FiClock className="w-4 h-4" />
-                            <p className='ml-1'>23 hours ago</p>
+                            <p className='ml-1'>{data.jobPost}</p>
                         </div>
                         <div className='flex text-gray-600 items-center'>
                             <FiBriefcase className="w-4 h-4" />
-                            <p className='ml-1'>Full-time</p>
+                            <p className='ml-1'>{data.type}</p>
                         </div>
                     </div>
                     <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FaMinus className="w-4 h-4" />
-                            <p className='ml-1'>No degree mentioned</p>
+                            <p className='ml-1'>{data.extensions[0]}</p>
                         </div>
                         <div className='flex text-gray-600 items-center'>
                             <FaShieldAlt className="w-4 h-4" />
-                            <p className='ml-1'>Health insurance</p>
+                            <p className='ml-1'>{data.extensions[1]}</p>
                         </div>
                     </div>
                 </div>
@@ -39,28 +51,28 @@ const JobListings = () => {
             <div className="flex border shadow-xl p-4 rounded-lg bg-gray-50 hover:bg-white">
                 <div className="w-16 h-16 bg-green-200"></div>
                 <div className="ml-5">
-                    <h2 className="font-extrabold">Barista</h2>
+                    <h2 className="font-extrabold">{data.title}</h2>
                     <p>Eleven Madison Park</p>
                     <p className='text-gray-600'>New York, NY</p>
-                    <p className='mb-4 text-gray-600'>via Indeed</p>
+                    <p className='mb-4 text-gray-600'>{data.via}</p>
                     <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FiClock className="w-4 h-4" />
-                            <p className='ml-1'>23 hours ago</p>
+                            <p className='ml-1'>{data.jobPost}</p>
                         </div>
                         <div className='flex text-gray-600 items-center'>
                             <FiBriefcase className="w-4 h-4" />
-                            <p className='ml-1'>Full-time</p>
+                            <p className='ml-1'>{data.title}</p>
                         </div>
                     </div>
                     <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FaMinus className="w-4 h-4" />
-                            <p className='ml-1'>No degree mentioned</p>
+                            <p className='ml-1'>{data.extensions[0]}</p>
                         </div>
                         <div className='flex text-gray-600 items-center'>
                             <FaShieldAlt className="w-4 h-4" />
-                            <p className='ml-1'>Health insurance</p>
+                            <p className='ml-1'>data.extensions[0]}></p>/p>
                         </div>
                     </div>
                 </div>
@@ -70,28 +82,28 @@ const JobListings = () => {
             <div className="flex border shadow-xl p-4 rounded-lg bg-gray-50 hover:bg-white">
                 <div className="w-16 h-16 bg-green-200"></div>
                 <div className="ml-5">
-                    <h2 className="font-extrabold">Barista</h2>
+                    <h2 className="font-extrabold">{data.title}</h2>
                     <p>Eleven Madison Park</p>
                     <p className='text-gray-600'>New York, NY</p>
-                    <p className='mb-4 text-gray-600'>via Indeed</p>
+                    <p className='mb-4 text-gray-600'>{data.via}</p>
                     <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FiClock className="w-4 h-4" />
-                            <p className='ml-1'>23 hours ago</p>
+                            <p className='ml-1'>{data.jobPost}</p>
                         </div>
                         <div className='flex text-gray-600 items-center'>
                             <FiBriefcase className="w-4 h-4" />
-                            <p className='ml-1'>Full-time</p>
+                            <p className='ml-1'>{data.title}</p>
                         </div>
                     </div>
                     <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FaMinus className="w-4 h-4" />
-                            <p className='ml-1'>No degree mentioned</p>
+                            <p className='ml-1'>{data.extensions[0]}</p>
                         </div>
                         <div className='flex text-gray-600 items-center'>
                             <FaShieldAlt className="w-4 h-4" />
-                            <p className='ml-1'>Health insurance</p>
+                            <p className='ml-1'>data.extensions[0]}></p>/p>
                         </div>
                     </div>
                 </div>
@@ -101,28 +113,28 @@ const JobListings = () => {
             <div className="flex border shadow-xl p-4 rounded-lg bg-gray-50 hover:bg-white">
                 <div className="w-16 h-16 bg-green-200"></div>
                 <div className="ml-5">
-                    <h2 className="font-extrabold">Barista</h2>
+                    <h2 className="font-extrabold">{data.title}</h2>
                     <p>Eleven Madison Park</p>
                     <p className='text-gray-600'>New York, NY</p>
-                    <p className='mb-4 text-gray-600'>via Indeed</p>
+                    <p className='mb-4 text-gray-600'>{data.via}</p>
                     <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FiClock className="w-4 h-4" />
-                            <p className='ml-1'>23 hours ago</p>
+                            <p className='ml-1'>{data.jobPost}</p>
                         </div>
                         <div className='flex text-gray-600 items-center'>
                             <FiBriefcase className="w-4 h-4" />
-                            <p className='ml-1'>Full-time</p>
+                            <p className='ml-1'>{data.title}</p>
                         </div>
                     </div>
                     <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FaMinus className="w-4 h-4" />
-                            <p className='ml-1'>No degree mentioned</p>
+                            <p className='ml-1'>{data.extensions[0]}</p>
                         </div>
                         <div className='flex text-gray-600 items-center'>
                             <FaShieldAlt className="w-4 h-4" />
-                            <p className='ml-1'>Health insurance</p>
+                            <p className='ml-1'>data.extensions[0]}></p>/p>
                         </div>
                     </div>
                 </div>
@@ -132,28 +144,28 @@ const JobListings = () => {
             <div className="flex border shadow-xl p-4 rounded-lg bg-gray-50 hover:bg-white">
                 <div className="w-16 h-16 bg-green-200"></div>
                 <div className="ml-5">
-                    <h2 className="font-extrabold">Barista</h2>
+                    <h2 className="font-extrabold">{data.title}</h2>
                     <p>Eleven Madison Park</p>
                     <p className='text-gray-600'>New York, NY</p>
-                    <p className='mb-4 text-gray-600'>via Indeed</p>
+                    <p className='mb-4 text-gray-600'>{data.via}</p>
                     <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FiClock className="w-4 h-4" />
-                            <p className='ml-1'>23 hours ago</p>
+                            <p className='ml-1'>{data.jobPost}</p>
                         </div>
                         <div className='flex text-gray-600 items-center'>
                             <FiBriefcase className="w-4 h-4" />
-                            <p className='ml-1'>Full-time</p>
+                            <p className='ml-1'>{data.title}</p>
                         </div>
                     </div>
                     <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FaMinus className="w-4 h-4" />
-                            <p className='ml-1'>No degree mentioned</p>
+                            <p className='ml-1'>{data.extensions[0]}</p>
                         </div>
                         <div className='flex text-gray-600 items-center'>
                             <FaShieldAlt className="w-4 h-4" />
-                            <p className='ml-1'>Health insurance</p>
+                            <p className='ml-1'>data.extensions[0]}></p>/p>
                         </div>
                     </div>
                 </div>
@@ -163,28 +175,28 @@ const JobListings = () => {
             <div className="flex border shadow-xl p-4 rounded-lg bg-gray-50 hover:bg-white">
                 <div className="w-16 h-16 bg-green-200"></div>
                 <div className="ml-5">
-                    <h2 className="font-extrabold">Barista</h2>
+                    <h2 className="font-extrabold">{data.title}</h2>
                     <p>Eleven Madison Park</p>
                     <p className='text-gray-600'>New York, NY</p>
-                    <p className='mb-4 text-gray-600'>via Indeed</p>
+                    <p className='mb-4 text-gray-600'>{data.via}</p>
                     <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FiClock className="w-4 h-4" />
-                            <p className='ml-1'>23 hours ago</p>
+                            <p className='ml-1'>{data.jobPost}</p>
                         </div>
                         <div className='flex text-gray-600 items-center'>
                             <FiBriefcase className="w-4 h-4" />
-                            <p className='ml-1'>Full-time</p>
+                            <p className='ml-1'>{data.title}</p>
                         </div>
                     </div>
                     <div className='flex gap-4 text-sm justify-center'>
                         <div className='flex text-gray-600 items-center'>
                             <FaMinus className="w-4 h-4" />
-                            <p className='ml-1'>No degree mentioned</p>
+                            <p className='ml-1'>{data.extensions[0]}</p>
                         </div>
                         <div className='flex text-gray-600 items-center'>
                             <FaShieldAlt className="w-4 h-4" />
-                            <p className='ml-1'>Health insurance</p>
+                            <p className='ml-1'>data.extensions[0]}></p>/p>
                         </div>
                     </div>
                 </div>
